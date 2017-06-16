42691cadb411eb495098a54535d82e4ce108105f
Jonas Scherer <jonas@matilhaestud.io>
2017-06-15 23:19:55 -0300

    first version

---
+++ app.js
@@ -0,0 +1,86 @@
+'use strict';
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Chat App Module
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+var basicChat = angular.module( 'BasicChat', ['chat'] );
+
+angular.module('chat').constant( 'config', {
+    rltm: {
+        service: 'pubnub', 
+        config: {
+            publishKey: 'demo',
+            subscribeKey: 'demo'
+        }
+    }
+});
+
+// or use socket.io
+// make sure to run socket.io-server from rltm.js
+// angular.module('chat').constant( 'config', {
+//     rltm: {
+//         service: 'socketio', 
+//         config: {
+//             endpoint: 'http://localhost:9000'
+//         }
+//     }
+// });
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Chat App Controller
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+basicChat.controller( 'BasicController', ['$scope', 'Messages', function($scope, Messages) {
+
+    // Sent Indicator
+    $scope.status = "";
+
+    // Keep an Array of Messages
+    $scope.messages = [];
+
+    $scope.me = {name: sillyname()};
+
+    // Set User Data
+    Messages.user($scope.me);
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Get Received Messages and Add it to Messages Array.
+    // This will automatically update the view.
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    var chatmessages = document.querySelector(".chat-messages");
+
+    Messages.receive(function(msg) {
+        console.log(msg)
+        $scope.messages.push(msg);
+    
+        setTimeout(function() {
+            chatmessages.scrollTop = chatmessages.scrollHeight;
+        }, 10);
+
+    });
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Send Messages
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    $scope.send = function() {
+
+        Messages.send({data: $scope.textbox});
+        
+        $scope.status = "sending";
+        $scope.textbox = "";
+
+        setTimeout(function() { 
+            $scope.status = "" 
+        }, 1200 );
+
+        var obj = {}
+        obj.data = "hey"
+        obj.user = {}
+        obj.user.id = "none"
+        obj.user.name = ""
+        $scope.status = "sending";
+        $scope.messages.push(obj);
+
+    };
+
+} ] );
